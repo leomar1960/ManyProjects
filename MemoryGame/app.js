310f0e84baf42280b1b46a4dfaa0bcfc5eba2487
leomar1960 <leo.maratsis@gmail.com>
2022-05-25 00:04:23 +0100

    update

---
+++ MemoryGame/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+	//card options
+	const cardsArray = [
+		{
+			name: "fries",
+			img: "./images/fries.png",
+		},
+		{
+			name: "fries",
+			img: "./images/fries.png",
+		},
+		{
+			name: "cheeseburger",
+			img: "./images/cheeseburger.png",
+		},
+		{
+			name: "cheeseburger",
+			img: "./images/cheeseburger.png",
+		},
+	];
+});
