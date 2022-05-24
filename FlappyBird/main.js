29e11b34411c91eab19c5dc229afe2e76ba5b29e
leomar1960 <leo.maratsis@gmail.com>
2022-05-24 18:51:06 +0100

    another update

--- FlappyBird/main.js
+++ FlappyBird/main.js
@@ -1,24 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const bird = document.querySelector(".bird");
-  const gameDispaly = document.querySelector(".game-container");
-  const ground = document.querySelector(".ground");
+	const bird = document.querySelector(".bird");
+	const gameDisplay = document.querySelector(".game-container");
+	const ground = document.querySelector(".ground");
 
-  let birdLeft = 220;
-  let birdBottom = 100;
-  let gravity = 2;
+	let birdLeft = 220;
+	let birdBottom = 100;
+	let gravity = 2;
+	let isGameOver = false;
 
-  function startGame() {
-    birdBottom -= gravity;
-    console.log(birdBottom);
-    bird.style.bottom = birdBottom + "px";
-    bird.style.left = birdLeft + "px";
-  }
+	function startGame() {
+		birdBottom -= gravity;
+		bird.style.bottom = birdBottom + "px";
+		bird.style.left = birdLeft + "px";
+	}
 
-  //   let timerId = setInterval(startGame, 20);
+	let gameTimerId = setInterval(startGame, 20);
 
-  function jump() {
-    birdBottom += 50;
-    bird.style.bottom = birdBottom + "px";
-  }
-  document.addEventListener("keyup", jump);
+	function control(e) {
+		if (e.keyCode === 32) {
+			jump();
+		}
+	}
+
+	function jump() {
+		if (birdBottom < 500) birdBottom += 50;
+		bird.style.bottom = birdBottom + "px";
+	}
+	document.addEventListener("keyup", control);
+
+	function generateObstacles() {
+		let obstacleLeft = 500;
+		let randomheight = Math.random() * 60;
+		let obstacleBottom = randomheight;
+		const obstacle = document.createElement("div");
+
+		if (!isGameOver) obstacle.classList.add("obstacle");
+		gameDisplay.appendChild(obstacle);
+		obstacle.style.left = obstacleLeft + "px";
+		obstacle.style.bottom = obstacleBottom + "px";
+
+		function moveObstacle() {
+			obstacleLeft -= 2;
+			obstacle.style.left = obstacleLeft + "px";
+
+			if (obstacleLeft === -60) {
+				clearInterval(timerId);
+				gameDisplay.removeChild(obstacle);
+			}
+
+			if (
+				(obstacleLeft > 200 &&
+					obstacleLeft < 280 &&
+					birdLeft === 220 &&
+					birdBottom < obstacleBottom + 153) ||
+				birdBottom === 0
+			) {
+				gameOver();
+				clearInterval(timerId);
+			}
+		}
+
+		let timerId = setInterval(moveObstacle, 20);
+		if (!isGameOver) setInterval(generateObstacles, 3000);
+	}
+
+	generateObstacles();
+
+	function gameOver() {
+		clearInterval(gameTimerId);
+		console.log("gameover");
+		isGameOver = true;
+		document.removeEventListener("keyup", control);
+	}
 });
