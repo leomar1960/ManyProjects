73b0b7588c9fedacff7b0559e18843edf4e2ead2
leomar1960 <leo.maratsis@gmail.com>
2022-05-25 22:12:03 +0100

    update:

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -54,9 +54,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	const grid = document.querySelector(".grid");
 	const resultDisplay = document.querySelector("#result");
-	let cardsChosen = [];
-	let cardsChosenId = [];
-	let cardsWon = [];
+	let cardsChosen = []; // here we add the name of image clicked by using the data-id
+	let cardsChosenId = []; //here we push the id of the ID of the clicked element
+	let cardsWon = []; // we push the array with the names of the cards won
 
 	// create you board
 
@@ -77,8 +77,9 @@ document.addEventListener("DOMContentLoaded", () => {
 		const optionTwoId = cardsChosenId[1];
 
 		if (optionOneId == optionTwoId) {
+			//if you click the same image
 			cards[optionOneId].setAttribute("src", "images/blank.png");
-			cards[optionTwoId].setAttribute("src", "images/blank.png");
+			// cards[optionTwoId].setAttribute("src", "images/blank.png");
 			alert("You have clicked the same image!");
 		} else if (cardsChosen[0] === cardsChosen[1]) {
 			alert("You found a match");
@@ -87,6 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			cards[optionOneId].removeEventListener("click", flipCard);
 			cards[optionTwoId].removeEventListener("click", flipCard);
 			cardsWon.push(cardsChosen);
+			console.log(cardsChosen);
 		} else {
 			cards[optionOneId].setAttribute("src", "images/blank.png");
 			cards[optionTwoId].setAttribute("src", "images/blank.png");
@@ -96,6 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		cardsChosen = [];
 		cardsChosenId = [];
 		resultDisplay.textContent = cardsWon.length;
+		console.log(cardsWon);
 		if (cardsWon.length === cardsArray.length / 2) {
 			resultDisplay.textContent = "Congratulation You found them all!!";
 		}
@@ -106,7 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		let cardId = this.getAttribute("data-id");
 		cardsChosen.push(cardsArray[cardId].name);
 		cardsChosenId.push(cardId);
-		this.setAttribute("src", cardsArray[cardId].img);
+		this.setAttribute("src", cardsArray[cardId].img); // we set again the source address of the image.
 		if (cardsChosen.length === 2) {
 			setTimeout(checkForMatch, 500);
 		}
